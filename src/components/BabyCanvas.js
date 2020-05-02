fb99a609f5b01e25e7421b9cac4c3b72411f5774
Jean-Pierre Lucar <jeanpierre.lucar@mercatus.com>
2020-05-02 15:47:38 -0400

    Add generated kid summary

--- src/components/BabyCanvas.js
+++ src/components/BabyCanvas.js
@@ -49,42 +49,42 @@ class BabyCanvas extends React.Component {
   renderGenotype = ( genotype ) => {
     if ( genotype.name === 'Sex' ) {
       if ( genotype.inheritedGeneName === 'Male' ) {
-        return <i className="huge male icon"></i>;
+        return <i key={ genotype.id } className="huge male icon"></i>;
       }
       else
-        return <i className="huge female icon"></i>;
+        return <i key={ genotype.id } className="huge female icon"></i>;
     } else if ( genotype.name === 'Face Shape' ) {
       if ( genotype.inheritedGeneName === 'Round' )
-        return <RoundFace className="face-shape" style={{ position: 'absolute', top: 100, left: 100, zIndex: 1 }} />;
+        return <RoundFace key={ genotype.id } className="face-shape" style={{ position: 'absolute', top: 100, left: 100, zIndex: 1 }} />;
       else
-        return <SquareFace className="face-shape" style={{ position: 'absolute', top: 100, left: 98, zIndex: 1 }} />;
+        return <SquareFace key={ genotype.id } className="face-shape" style={{ position: 'absolute', top: 100, left: 98, zIndex: 1 }} />;
     } else if ( genotype.name === 'Presence of Chin Cleft' ) {
       if ( genotype.inheritedGeneName === 'Cleft Chin' )
-        return <CleftChin className="cleft-chin" style={{ position: 'absolute', top: 232, left: 145, zIndex: 2 }} />;
+        return <CleftChin key={ genotype.id } className="cleft-chin" style={{ position: 'absolute', top: 232, left: 145, zIndex: 2 }} />;
       else
         return null;
     } else if ( genotype.name === 'Hair Type' ) {
       if ( genotype.inheritedGeneName === 'Curly') {
         if ( this.props.config.isMale )
-          return <ManCurlyHair className="hair" style={{ position: 'absolute', top: 66, left: 86, zIndex: 1 }} />;
+          return <ManCurlyHair key={ genotype.id } className="hair" style={{ position: 'absolute', top: 66, left: 86, zIndex: 1 }} />;
         else
-          return <WomanCurlyHair className="hair" style={{ position: 'absolute', top: 66, left: 86, zIndex: 0 }} />;
+          return <WomanCurlyHair key={ genotype.id } className="hair" style={{ position: 'absolute', top: 66, left: 86, zIndex: 0 }} />;
       } else if ( genotype.inheritedGeneName === 'Wavy') {
         if ( this.props.config.isMale )
-          return <ManWavyHair className="hair" style={{ position: 'absolute', top: 66, left: 86, zIndex: 1 }} />;
+          return <ManWavyHair key={ genotype.id } className="hair" style={{ position: 'absolute', top: 66, left: 86, zIndex: 1 }} />;
         else
-          return <WomanWavyHair className="hair" style={{ position: 'absolute', top: 78, left: 70, zIndex: 0 }} />;
+          return <WomanWavyHair key={ genotype.id } className="hair" style={{ position: 'absolute', top: 78, left: 70, zIndex: 0 }} />;
       } else {
         if ( this.props.config.isMale )
-          return <ManStraightHair className="hair" style={{ position: 'absolute', top: 66, left: 102, zIndex: 1 }} />;
+          return <ManStraightHair key={ genotype.id } className="hair" style={{ position: 'absolute', top: 66, left: 102, zIndex: 1 }} />;
         else
-          return <WomanStraightHair className="hair" style={{ position: 'absolute', top: 76, left: 64, zIndex: 0 }} />;
+          return <WomanStraightHair key={ genotype.id } className="hair" style={{ position: 'absolute', top: 76, left: 64, zIndex: 0 }} />;
       }
     } else if ( genotype.name === 'Eye Shape' ) {
       if ( genotype.inheritedGeneName === 'Almond') {
         if ( this.props.config.isMale ) {
           return (
-            <div>
+            <div key={ genotype.id }>
               <ManAlmondEyeShape style={{ position: 'absolute', top: 140, left: 112, zIndex: 4 }} />
               <ManAlmondEyeShape style={{ position: 'absolute', top: 140, left: 180, transform: 'scaleX( -1 )', zIndex: 4 }} />
             </div>
@@ -92,7 +92,7 @@ class BabyCanvas extends React.Component {
         }
         else {
           return (
-            <div>
+            <div key={ genotype.id }>
               <WomanAlmondEyeShape style={{ position: 'absolute', top: 139, left: 101, zIndex: 4 }} />
               <WomanAlmondEyeShape style={{ position: 'absolute', top: 139, left: 182, transform: 'scaleX( -1 )', zIndex: 4 }} />
             </div>
@@ -101,7 +101,7 @@ class BabyCanvas extends React.Component {
       } else {
         if ( this.props.config.isMale ) {
           return (
-            <div>
+            <div key={ genotype.id }>
               <ManRoundEyeShape style={{ position: 'absolute', top: 140, left: 112, zIndex: 4 }} />
               <ManRoundEyeShape style={{ position: 'absolute', top: 140, left: 182, transform: 'scaleX( -1 )', zIndex: 4 }} />
             </div>
@@ -109,7 +109,7 @@ class BabyCanvas extends React.Component {
         }
         else {
           return (
-            <div>
+            <div key={ genotype.id }>
               <WomanRoundEyeShape style={{ position: 'absolute', top: 140, left: 109, height: '27px', zIndex: 4 }} />
               <WomanRoundEyeShape style={{ position: 'absolute', top: 140, left: 179, height: '27px', transform: 'scaleX( -1 )', zIndex: 4 }} />
             </div>
@@ -119,14 +119,14 @@ class BabyCanvas extends React.Component {
     } else if ( genotype.name === 'Eye Colour' ) {
       if ( genotype.inheritedGeneName === 'Brown') {
         return (
-          <div>
+          <div key={ genotype.id }>
             <Eye style={{ position: 'absolute', top: 142, left: 125, width: '22px', zIndex: 5 }} />
             <Eye style={{ position: 'absolute', top: 142, left: 193, width: '22px', zIndex: 5 }} />
           </div>
         );
       } else {
         return (
-          <div>
+          <div key={ genotype.id }>
             <Eye className="eye-blue" style={{ position: 'absolute', top: 142, left: 125, width: '22px', zIndex: 5 }} />
             <Eye className="eye-blue" style={{ position: 'absolute', top: 142, left: 193, width: '22px', zIndex: 5 }} />
           </div>
@@ -136,7 +136,7 @@ class BabyCanvas extends React.Component {
       if ( genotype.inheritedGeneName === 'Thick') {
         if ( this.props.config.isMale ) {
           return (
-            <div>
+            <div key={ genotype.id }>
               <ManThickEyebrow style={{ position: 'absolute', top: 130, left: 110, zIndex: 6 }} />
               <ManThickEyebrow style={{ position: 'absolute', top: 130, left: 178, transform: 'scaleX( -1 )', zIndex: 6 }} />
             </div>
@@ -144,7 +144,7 @@ class BabyCanvas extends React.Component {
         }
         else {
           return (
-            <div>
+            <div key={ genotype.id }>
               <WomanThickEyebrow className="eyebrow" style={{ position: 'absolute', top: 130, left: 106, zIndex: 6 }} />
               <WomanThickEyebrow className="eyebrow" style={{ position: 'absolute', top: 130, left: 176, transform: 'scaleX( -1 )', zIndex: 6 }} />
             </div>
@@ -153,7 +153,7 @@ class BabyCanvas extends React.Component {
       } else {
         if ( this.props.config.isMale ) {
           return (
-            <div>
+            <div key={ genotype.id }>
               <ManThinEyebrow style={{ position: 'absolute', top: 130, left: 112, zIndex: 6 }} />
               <ManThinEyebrow style={{ position: 'absolute', top: 130, left: 182, transform: 'scaleX( -1 )', zIndex: 6 }} />
             </div>
@@ -161,7 +161,7 @@ class BabyCanvas extends React.Component {
         }
         else {
           return (
-            <div>
+            <div key={ genotype.id }>
               <WomanThinEyebrow style={{ position: 'absolute', top: 130, left: 112, zIndex: 6 }} />
               <WomanThinEyebrow style={{ position: 'absolute', top: 130, left: 182, transform: 'scaleX( -1 )', zIndex: 6 }} />
             </div>
@@ -171,7 +171,7 @@ class BabyCanvas extends React.Component {
     } else if ( genotype.name === 'Eye Lashes' ) {
       if ( genotype.inheritedGeneName === 'Long' ) {
         return (
-          <div>
+          <div key={ genotype.id }>
             <LongEyelashes style={{ position: 'absolute', top: 130, left: 102, zIndex: 7 }} />
             <LongEyelashes style={{ position: 'absolute', top: 130, left: 216, transform: 'scaleX( -1 )', zIndex: 7 }} />
           </div>
@@ -181,25 +181,25 @@ class BabyCanvas extends React.Component {
       }
     } else if ( genotype.name === 'Nose Shape' ) {
       if ( genotype.inheritedGeneName === 'Rounded' )
-        return <RoundedNoseShape className="nose-shape" style={{ position: 'absolute', top: 166, left: 155, zIndex: 8 }} />;
+        return <RoundedNoseShape key={ genotype.id } className="nose-shape" style={{ position: 'absolute', top: 166, left: 155, zIndex: 8 }} />;
       else
-        return <PointedNoseShape className="nose-shape" style={{ position: 'absolute', top: 166, left: 155, zIndex: 8 }} />;
+        return <PointedNoseShape key={ genotype.id } className="nose-shape" style={{ position: 'absolute', top: 166, left: 155, zIndex: 8 }} />;
     } else if ( genotype.name === 'Lip Thickness' ) {
       if ( genotype.inheritedGeneName === 'Thick' )
-        return <ThickLips style={{ position: 'absolute', top: 198, left: 148, zIndex: 9 }} />;
+        return <ThickLips key={ genotype.id } style={{ position: 'absolute', top: 198, left: 148, zIndex: 9 }} />;
       else
-        return <ThinLips style={{ position: 'absolute', top: 198, left: 148, zIndex: 9 }} />;
+        return <ThinLips key={ genotype.id } style={{ position: 'absolute', top: 198, left: 148, zIndex: 9 }} />;
     } else if ( genotype.name === 'Earlobe Attachment' ) {
       if ( genotype.inheritedGeneName === 'Free' ){
         return (
-          <div>
+          <div key={ genotype.id }>
             <FreeEarlobe className="earlobe" style={{ position: 'absolute', top: 148, left: 84, zIndex: 10 }} />
             <FreeEarlobe className="earlobe" style={{ position: 'absolute', top: 148, left: 228, transform: 'scaleX( -1 )', zIndex: 10 }} />
           </div>
         );
       } else {
         return (
-          <div>
+          <div key={ genotype.id }>
             <AttachedEarlobe className="earlobe" style={{ position: 'absolute', top: 148, left: 84, zIndex: 10 }} />
             <AttachedEarlobe className="earlobe" style={{ position: 'absolute', top: 148, left: 228, transform: 'scaleX( -1 )', zIndex: 10 }} />
           </div>
@@ -208,7 +208,7 @@ class BabyCanvas extends React.Component {
     } else if ( genotype.name === 'Freckles' ) {
       if ( genotype.inheritedGeneName === 'Freckles' ){
         return (
-          <div>
+          <div key={ genotype.id }>
             <Freckles className="freckles" style={{ position: 'absolute', top: 176, left: 114, zIndex: 11 }} />
             <Freckles className="freckles" style={{ position: 'absolute', top: 176, left: 194, transform: 'scaleX( -1 )', zIndex: 11 }} />
           </div>
@@ -247,17 +247,48 @@ class BabyCanvas extends React.Component {
     return this.props.genotypes.every( genotype => genotype.draw );
   }
 
+  renderSummary = () => {
+    return this.props.genotypes.map( genotype => {
+      if ( !genotype.draw ) return null;
+
+      if ( genotype.name === 'Sex' &&
+        ( genotype.fatherGene + genotype.motherGene === 'TT' ||
+        genotype.fatherGene + genotype.motherGene === 'tt' ) ) {
+          return (
+            <div key={ genotype.id }><span style={{ marginRight: '8px', fontWeight: '600' }}>{ genotype.name }:</span> { genotype.inheritedGeneName } ( XX )</div>
+          );
+        }
+
+      if ( genotype.name === 'Sex' &&
+        ( genotype.fatherGene + genotype.motherGene === 'Tt' ||
+        genotype.fatherGene + genotype.motherGene === 'tT' ) ) {
+          return (
+            <div key={ genotype.id }><span style={{ marginRight: '8px', fontWeight: '600' }}>{ genotype.name }:</span> { genotype.inheritedGeneName } ( XY )</div>
+          );
+        }
+
+      return (
+        <div key={ genotype.id } style={{ marginBottom: '2px' }}><span style={{ marginRight: '8px', fontWeight: '600' }}>{ genotype.name }:</span> { genotype.inheritedGeneName } ( { genotype.fatherGene + genotype.motherGene } )</div>
+      );
+    });
+  }
+
   render(){
     const className = `seven wide column ${ this.props.config.isMale ? 'sex-male' : 'sex-female' }  ${ this.props.config.hairColor ? 'hair-' + this.props.config.hairColor : '' }  ${ this.props.config.skinColor ? 'skin-' + this.props.config.skinColor : '' }`;
     return (
       <div className={ className } style={{ position: 'fixed', right: 0 }}>
         { this.allGenerated() ? (
-          <button className="ui primary button" onClick={ this.downloadGeneratedBaby }>Download Generated Kid</button>
+          <button className="ui right floated primary button" onClick={ this.downloadGeneratedBaby }>Download Generated Kid</button>
         )
         : null
         }
-        <div id="generated-kid" style={{ height: '100vh' }}>
-          { this.renderFace() }
+        <div id="generated-kid">
+          <div style={{ height: '30vh' }}>
+           { this.renderFace() }
+          </div>
+          <div>
+            { this.renderSummary() }
+          </div>
         </div>
       </div>
     );
